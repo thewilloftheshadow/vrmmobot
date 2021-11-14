e501af7e14cee11aca22ce3ae16ca8973281e18f
Shadow <william.shadow@outlook.com>
2021-11-14 13:02:12 -0600

    Update pickroles.js

--- commands/dev/pickroles.js
+++ commands/dev/pickroles.js
@@ -12,5 +12,5 @@ module.exports.run = async (message, args, client) => {
   message.channel.send({content: `Hey there! Welcome to the VR/MMO Church Mega-Server!\n** **`})
   message.channel.send({content: `**Ping Roles**\nThese roles can be pinged by anyone in the role's matching channel by simply using \`-ping\`.\nTo claim a role, simply click on the button for it!`, components: [config.buttons.rows.vrcp, config.buttons.rows.mmop]})
   //message.channel.send({content: `** **\n**Section Roles**\nBy default, you are only able to see the basic community channels. If you would like to gain access to the specialized channels for the different parts of the server, use the buttons below!`, components: [config.buttons.rows.sections]})
-  message.channel.send({content: `**Other Roles**\nThese roles are for misc things that we have in this server!`, components: [config.buttons.rows.other]})
+  message.channel.send({content: `**Other Roles**\nThese roles are for misc things that we have in this server!`, components: [config.buttons.rows.other, config.buttons.rows.calendar]})
 }
